6f3a2d93cce499041afa8a1a1713435f88e574f5
troat9824 <troat9824@gmail.com>
2022-02-26 01:38:12 -0600

    delete removes task

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,6 +2,7 @@ var taskIdCounter = 0;
 
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 var taskFormHandler = function(event) {
 
     event.preventDefault();
@@ -75,3 +76,20 @@ var createTaskActions = function(taskId) {
     return actionContainerEl;
 }
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function (event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        // get the element's task id
+        var taskId = event.target.getAttribute('data-task-id');
+        deleteTask(taskId);
+    };
+};
+
+// delete task
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+pageContentEl.addEventListener("click", taskButtonHandler);
